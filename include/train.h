090b272e1cd9ff21b874199e5a10d84f43f53fff
fakemove <160417424+fakemove@users.noreply.github.com>
2024-05-08 19:30:59 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,7 +1,6 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
-
 class Train {
  private:
   struct Cage {
@@ -13,6 +12,7 @@ class Train {
   Cage *first; // точка входа в поезд (первый вагон)
  public:
   Train();
+  ~Train();
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
